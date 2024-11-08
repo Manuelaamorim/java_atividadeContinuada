30ba505219c0f616a8f8e39f556094b11f54944f
Manuela Cavalcanti <manucamorim@hotmail.com>
2024-11-07 21:18:50 -0300

    add Junit e classe de testes

--- src/br/com/cesarschool/poo/titulos/daogenerico/DAOSerializadorObjetos.java
+++ src/br/com/cesarschool/poo/titulos/daogenerico/DAOSerializadorObjetos.java
@@ -1,5 +1,18 @@
 package br.com.cesarschool.poo.titulos.daogenerico;
 
+/*
+ * Esta classe representa um DAO genérico, que inclui, exclui, altera, busca por identificador
+ * único e busca todos, qualquer objeto(s) cujo tipo é subtipo de Entidade.
+ *
+ * Sugerimos o uso da API de serialização do JAVA, que grava e lê objetos em arquvos.
+ * Lembrar sempre de fechar (em qualquer circunstância) streams JAVA abertas.
+ *
+ * As nuances mais detalhadas do funcionamento desta classe está especificada na classe de testes
+ * automatizados br.gov.cesarschool.poo.testes.TestesDAOSerializador.
+ *
+ * A classe deve ter a estrutura (métodos e construtores) dada abaixo.
+ */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
